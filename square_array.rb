7a7890537e9ba706da184a5817a6b011bc35e485
brandonworks <work@brandontorres.dev>
2021-02-10 01:10:45 -0800

    commit tree d0d7b4a4144856e12393fe2cd718cd117de4b281

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+numbers = [1,2,3]
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each {|num| new_array << num*num}
+  new_array
 end
+
+puts square_array(numbers)
+
+new_numbers = [9,10,16,25]
+puts square_array(new_numbers)
